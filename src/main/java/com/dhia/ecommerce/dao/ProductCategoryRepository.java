397e3a16047723f47b01d68beab2243097635462
dhia saadi <saadidhia3@gmail.com>
2021-05-21 00:23:39 +0100

    adding general origin to Repositories

--- src/main/java/com/dhia/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/dhia/ecommerce/dao/ProductCategoryRepository.java
@@ -2,8 +2,10 @@ package com.dhia.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.dhia.ecommerce.entities.*;
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel="productCategory",path="product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
 
--- src/main/java/com/dhia/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/dhia/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.dhia.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.dhia.ecommerce.entities.*;
-
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product,Long> {
 	
 	
